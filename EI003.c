0c2fbaa7f1054dc3c1079d6648659beeeef1b45f
Pietro Dapporto <pietro.dapporto@studio.unibo.it>
2025-05-23 16:06:31 +0200

    Update EI003.c

--- EI003.c
+++ EI003.c
@@ -1,3 +1,4 @@
+
 /********************************************************************************
  *                                                                              *
  *                   Architetture dei sistemi di Elaborazione                   *
@@ -15,8 +16,8 @@
 void main()
 {
 	// Input
-	unsigned char vet[] = { 0xF2 };	// Sequenza di bit
-	unsigned short int len = 1;				// Lunghezza (in bit) della sequenza
+	unsigned char vet[] = { 0xBC,0xFF,0x01 };	// Sequenza di bit
+	unsigned short int len = 18;				// Lunghezza (in bit) della sequenza
 
 	// Output
 	short int minLung;		// Lunghezza minima sotto-sequenza di 0
@@ -24,142 +25,164 @@ void main()
 
 	__asm
 	{
-		; lunghezza massima
-XOR EAX, EAX; indice che scorre il vettore
-XOR ECX, ECX; numeri di bit da considerare
-XOR EDX, EDX; numero di bit a zero da confrontare
-XOR EBX, EBX; contatore dei bit nei singoli byte del vettore
-MOV CX, len
+		XOR EAX, EAX; indice sul vettore
+XOR ECX, ECX; minLung(CL)
+XOR EDX, EDX; maxLung(DL)
+XOR EBX, EBX; BH = contatore bit, BL = byte corrente
 
-MOV BL, vet[EAX]
+MOV BL, vet[EAX]; carica il primo byte
 
-ciclo1: INC BH
-CMP BH, 8; verifico la fine del primo byte
-JE nuovo_byte1
-TEST BL, 1
-JNZ passo_oltre1
-INC DL; contatore di zeri in fila
-SHR BL, 1
-LOOP ciclo1
-JMP fine1
+ciclo :
+MOV AL, BH
+	CMP AX, len; fine dei bit ?
+	JE L0
+	CMP BH, 0
+	JZ salta
+	TEST BH, 7; fine byte ?
+	JZ nuovo_byte
 
-passo_oltre1 : SHR BL, 1; 1 come ultimo bit, e sposto i bit
-CMP DH, DL
-JB da_cambiare1
-JMP intatto1
+	salta :
+INC BH
 
-da_cambiare1 : MOV DH, DL; inverto i registri se ottengo seguenza maggiore
-intatto1 : XOR DL, DL
-LOOP ciclo1
-JMP fine1
+	prova :
+	TEST BL, 1
+	JNZ passo_oltre
+	INC DH; incremento sequenza zeri
+	SHR BL, 1
+	JMP ciclo
 
-nuovo_byte1 : XOR BH, BH; passo al byte successivo
-INC EAX
-MOV BL, vet[EAX]
-LOOP ciclo1
+	passo_oltre :
+SHR BL, 1; scorro bit
 
-fine1 : CMP DH, 0
-JZ esci1
+	L0 :
+CMP DL, DH; confronta con max
+	JB L1
+	JMP L2
 
-XCHG DL, DH; scambio i registri per avere la sequenza in offset minore
+	L1 :
+MOV DL, DH; aggiorna max
+
+	L2 :
+CMP CL, 0
+	JZ L3
+	CMP DH, 0
+	JZ continua
+	CMP CL, DH; confronta con min
+	JB continua
+
+	L3 :
+MOV CL, DH; aggiorna min
+
+	continua :
 XOR DH, DH
+	MOV AL, BH
+	CMP AX, len; fine dei bit ?
+	JE fine
+	JMP ciclo
 
-esci1 : CMP DL, 0
-JE nulla1
-MOV maxLung, DX
-JMP basta1
+	nuovo_byte :
+INC CH; passo al byte
+	MOV AL, CH
+	MOV BL, vet[EAX]
+    INC BH
+	MOV AL, BH
+	CMP AX, len; fine dei bit ?
+	JE L0
+	JMP prova
 
-nulla1 : MOV maxLung, -1; non ci sono zeri
-
-basta1 :
-
-; lunghezza minima
-XOR EAX, EAX; indice che scorre il vettore
-XOR ECX, ECX; numeri di bit da considerare
-XOR EDX, EDX; numero di bit a zero da confrontare
-XOR EBX, EBX; contatore dei bit nei singoli byte del vettore
-MOV CX, len
-
-MOV BL, vet[EAX]
-
-ciclo2: INC BH
-CMP BH, 8; verifico la fine del primo byte
-JE nuovo_byte2
-TEST BL, 1
-JNZ passo_oltre2
-INC DL; contatore di zeri in fila
-SHR BL, 1
-LOOP ciclo2
-JMP fine2
-
-passo_oltre2 : SHR BL, 1; 1 come ultimo bit, e sposto i bit
+	fine :
 CMP DL, 0
-JZ intatto2
-CMP DH, 0
-JZ da_cambiare2
-CMP DH, DL
-JA da_cambiare2
-JMP intatto2
+	JZ esci
 
-da_cambiare2 : MOV DH, DL; inverto i registri se ottengo seguenza maggiore
-intatto2 : XOR DL, DL
-LOOP ciclo2
-JMP fine2
+	XOR CH, CH
+	MOV maxLung, DX
+	MOV minLung, CX
+	JMP basta
 
-nuovo_byte2 : XOR BH, BH; passo al byte successivo
-INC EAX
-MOV BL, vet[EAX]
-LOOP ciclo2
+	esci :
+	MOV maxLung, -1; caso senza zeri
+	MOV minLung, -1
 
-/*fine2 : CMP DH, 0
-JZ esci2
-
-CMP DL, DH
-JB basso2
-scambia2: XCHG DL, DH; scambio i registri per avere la sequenza in offset minore
-JMP stampa
-
-esci2: CMP DL, 0
-JE nulla2
-basso2: CMP DL, 0
-JZ scambia2
-stampa: XOR DH, DH
-MOV minLung, DX
-JMP basta2
-
-nulla2: MOV minLung, -1; non ci sono zeri
-
-basta2:*/
-
-fine2:
-CMP DH, 0
-	JZ esci2; se nessuna sequenza salvata
-
-	CMP DL, DH
-	JB scambia2; se DL < DH → è la sequenza minima → scambia
-	JMP stampa; altrimenti stampa DH come minimo
-
-	esci2 :
-CMP DL, 0
-	JE nulla2; nessuna sequenza
-	JMP stampa; altrimenti usa DL
-
-	scambia2 :
-XCHG DL, DH; scambia per avere DL come minimo
-
-	stampa :
-XOR DH, DH; azzera DH per sicurezza
-	MOV minLung, DX; salva la lunghezza minima trovata
-	JMP basta2
-
-	nulla2 :
-MOV minLung, -1; nessuna sequenza di 0 trovata
-
-	basta2 :
-		
+	basta :
 	}
 
 	// Stampa su video
 	printf("Lunghezza minima delle sotto-sequenze di 0: %d\n", minLung);
 	printf("Lunghezza massima delle sotto-sequenze di 0: %d\n", maxLung);
 }
+
+/*XOR EAX, EAX              ; indice nel vettore
+    XOR ECX, ECX              ; CL = minLung
+    XOR EDX, EDX              ; DL = maxLung
+    XOR EBX, EBX              ; BH = contatore bit, BL = byte corrente
+
+    MOV BL, vet[EAX]          ; carica primo byte
+    MOV CX, len               ; numero di bit da analizzare
+
+ciclo:
+    CMP BH, CX
+    JE fine                   ; se letti tutti i bit, esci
+
+    CMP BH, 0
+    JZ salta
+    TEST BH, 7
+    JZ nuovo_byte             ; ogni 8 bit → nuovo byte
+
+salta:
+    INC BH                    ; incrementa contatore bit
+
+prova:
+    TEST BL, 1
+    JNZ passo_oltre           ; bit = 1 → fine sequenza zeri
+
+    INC DH                    ; conta zeri consecutivi
+    SHR BL, 1                 ; prossimo bit
+    JMP ciclo
+
+passo_oltre:
+    SHR BL, 1
+
+    CMP DL, DH
+    JB L1                     ; aggiorna max se DH > DL
+    JMP L2
+
+L1:
+    MOV DL, DH
+
+L2:
+    CMP CL, 0
+    JZ L3
+    CMP DH, 0
+    JZ continua
+    CMP CL, DH
+    JB continua
+
+L3:
+    MOV CL, DH                ; aggiorna min se serve
+
+continua:
+    XOR DH, DH                ; reset contatore zeri
+    CMP BH, CX
+    JE fine
+    JMP ciclo
+
+nuovo_byte:
+    INC EAX                   ; byte successivo
+    MOV BL, vet[EAX]
+    INC BH
+    JMP prova
+
+fine:
+    CMP DL, 0
+    JZ esci                   ; se nessuno zero trovato
+
+    XOR CH, CH
+    MOV word ptr [maxLung], DX
+    MOV word ptr [minLung], CX
+    JMP basta
+
+esci:
+    MOV word ptr [maxLung], -1
+    MOV word ptr [minLung], -1
+
+basta:*/
